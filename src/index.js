c507f3b7972be40362452f1f560fdd97a1d4a8cc
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2019-08-13 20:59:19 +0200

    Splitted resolvers into files, added signup and login resolvers, completed authentication

--- src/index.js
+++ src/index.js
@@ -2,35 +2,27 @@ require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client');
 
-let links = [{
-    id: 'link-0',
-    url: 'www.google.com',
-    description: 'whateva',
-}];
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
-let idCount = links.length;
 const resolvers = {
-    Query: {
-        info: () => 'Api working',
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-        link: (parent, args) => links.find(link => args.id === link.id),
-    },
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        }
-    },
+    Query,
+    Mutation,
+    User,
+    Link,
 };
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        };
+    },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
