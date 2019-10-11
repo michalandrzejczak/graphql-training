adb0afc62f1ae3c7a4ebea77d7781488a7741e15
Michał Andrzejczak <michal.andrzejcz@gmail.com>
2019-10-11 12:41:59 +0200

    Added websockets, live subscriptions of adding new link and votes to links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,7 +4,7 @@ import gql from "graphql-tag";
 
 import Link from "./Link.js";
 
-export const LINKS_QUERY = gql(`
+export const LINKS_QUERY = gql`
     {
         feed (orderBy: createdAt_DESC) {
             links {
@@ -25,15 +25,101 @@ export const LINKS_QUERY = gql(`
             }
         }
     }
-`);
+`;
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            user {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+      newVote {
+          id,
+          link {
+              id
+              url
+              description
+              user {
+                  id
+                  name
+              }
+              votes {
+                  id
+                  user {
+                      id
+                  }
+              }
+          }
+          user {
+              id
+          }
+      }
+  } 
+`;
 
 class LinkList extends Component {
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({query: LINKS_QUERY});
+
+        data.feed.links.find(link => link.id === linkId).votes.push(createVote);
+
+        store.writeQuery({query: LINKS_QUERY, data});
+    };
+
+    _subscribeToNewLinks = (subscribeToMore) => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, {subscriptionData}) => {
+                if (!subscriptionData.data) return prev;
+
+                const newLink = subscriptionData.data.newLink;
+                const exists = prev.feed.links.find(({id}) => id === newLink.id);
+
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename,
+                    },
+                })
+            },
+        })
+    };
+
+    _subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_VOTES_SUBSCRIPTION
+        })
+    };
+
     render() {
         return (
             <Query query={LINKS_QUERY}>
-                {({loading, error, data}) => {
+                {({loading, error, data, subscribeToMore}) => {
                     if (loading) return <div className="ma3 measure">Loading...</div>;
                     if (error) return <div className="dark-red">Unexpected error!</div>;
+
+                    this._subscribeToNewLinks(subscribeToMore);
+                    this._subscribeToNewVotes(subscribeToMore);
 
                     const linksToRender = data.feed.links;
 
@@ -42,7 +128,8 @@ class LinkList extends Component {
                             <p className="f4 fw6 ph0 mh0">Link list</p>
                             <ul className="list pl0">
                                 {linksToRender.map((link, index) => (
-                                    <Link key={link.id} link={link} index={index} updateStoreAfterVote={this._updateCacheAfterVote}/>
+                                    <Link key={link.id} link={link} index={index}
+                                          updateStoreAfterVote={this._updateCacheAfterVote}/>
                                 ))}
                             </ul>
                         </div>
@@ -50,14 +137,6 @@ class LinkList extends Component {
                 }}
             </Query>
         );
-    }
-
-    _updateCacheAfterVote = (store, createVote, linkId) => {
-        const data = store.readQuery({query: LINKS_QUERY});
-
-        data.feed.links.find(link => link.id === linkId).votes.push(createVote);
-
-        store.writeQuery({query: LINKS_QUERY, data});
     }
 }
 
